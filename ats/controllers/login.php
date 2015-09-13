4c93cb16dc424f79db8bb2c4d5bde99025b3c676
Anthony Zheng <tamakimouto@gmail.com>
2015-09-13 17:33:15 -0400

    testing

--- ats/controllers/login.php
+++ ats/controllers/login.php
@@ -1,22 +1,28 @@
 <?php
- include('../dbHelpers/LoginCheck.php');
- $username = $_POST["username"];
- $password = $_POST["password"];
- $account = $_POST["account"];
- $logincheck;
- $errorMessage;
- $loginAttempts=0;
- //$loginAttempts.=$_SESSION['loginAttempts'];
- $url;
+include('../dbHelpers/LoginCheck.php');
+$username = $_POST["username"];
+$password = $_POST["password"];
+$account = $_POST["account"];
+$logincheck;
+$errorMessage;
+$loginAttempts = 0;
+//$loginAttempts.=$_SESSION['loginAttempts'];
+$url;
 
-  if($loginAttempts > 2){														//exceeded logins
-             $errorMessage = "<div><p>Error: Number of Login Attempts Exceeded</p></div>";
-             session_start();
-             $_SESSION['errorMessage'] = $errorMessage;
-             header('Location: ../index.php');
-        } else {
-        if($account==="login"){
-        $logincheck = new LoginCheck("localhost","application","root", "", $username, $password);
+if($loginAttempts > 2) {		//exceeded logins
+
+    $errorMessage = "<div><p>Error: Number of Login Attempts Exceeded</p></div>";
+    session_start();
+    $_SESSION['errorMessage'] = $errorMessage;
+    header('Location: ../index.php');
+} else {
+    if($account==="login"){
+        $logincheck = new LoginCheck("localhost",
+                                     "application",
+                                     "root",
+                                     "",
+                                     $username, $password);
+
         if($logincheck->doCheck() == true){
         	echo "fetched successfully";
         	session_start();
@@ -34,27 +40,29 @@
         	$_SESSION['telephone'] = $logincheck->profile->getTelephone();
         	$_SESSION['email'] = $logincheck->profile->getEmail();
         	$_SESSION['application'] = $logincheck->profile->getApplication();
-        	
-        	if($logincheck->profile->getType()==="admin"){header('Location: browseC.php');} else {header('Location: settingsC.php');}
 
+        	if($logincheck->profile->getType()==="admin") {
+                header('Location: browseC.php');
+            } else {
+                header('Location: settingsC.php');
+            }
         } else {
             session_start();
             $loginAttempts++;
-            $foo = $_SESSION['loginAttempts'];
+            //$foo = $_SESSION['loginAttempts'];
             settype($foo, "integer");
-            $foo.=$loginAttempts;
-            $errorMessage = "Error: Unrecognized Username or Password, You have " . (3-$foo) . " Login Attempts Left";
-            
+            //$foo.=$loginAttempts;
+            $errorMessage = "Error: Unrecognized Username or Password, You have " . (3 - $loginAttempts) . " Login Attempts Left";
+
             $_SESSION['errorMessage'] = $errorMessage;
             header('Location: ../index.php');
         }
-        
+
         //header('Location: ../index.php');
-        } else {
-            header('Location: AddAccountC.php');
+    } else {
+        header('Location: AddAccountC.php');
 
-        }
-
-        }
+    }
+}
 
 ?>
